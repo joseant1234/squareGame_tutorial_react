aaa7294e03db69e1c31586de6665395bd65f18a3
joseant1234 <j-osea-nt@hotmail.com>
2018-06-08 11:13:33 -0500

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,12 +9,33 @@ function Square(props){
   )
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default class Board extends React.Component{
 
   constructor(props){
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
@@ -28,12 +49,25 @@ export default class Board extends React.Component{
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    // valida si ya un ganador o si se hizo click sobre el mismo elemento se debe hacer return evitando el click
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render(){
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+    }
 
     return(
       <div>
